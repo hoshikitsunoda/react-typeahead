da6f461b2ad7a46aa64ac13a79acf633056c7f08
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-11-15 15:38:45 -0800

    Add search bar component to render input box to html. #1

--- client/public/main.js
+++ client/public/main.js
@@ -1,3 +1,13 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
 import styled from 'styled-components'
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <input type="text" placeholder="search" />
+    )
+  }
+}
+
+render(<SearchBar />, document.querySelector('#app'))
