f12edf36364a5c20bc6998c097a64fa52d7f99f0
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-11-17 09:33:42 -0800

    Add component to show matched data. #1

--- client/public/main.js
+++ client/public/main.js
@@ -1,13 +1,104 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
-import styled from 'styled-components'
 
-class SearchBar extends React.Component {
+const show = { display: 'block' }
+const hide = { display: 'none' }
+
+class Names extends React.Component {
   render() {
+    const filterText = this.props.filterText
+    const name = this.props.name
     return (
-      <input type="text" placeholder="search" />
+      <li>{name}</li>
     )
   }
 }
 
-render(<SearchBar />, document.querySelector('#app'))
+class NameTable extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      names: []
+    }
+  }
+
+  componentWillMount() {
+    fetch('/typeaheadData')
+      .then(res => res.json())
+      .then(names => this.setState({names}))
+  }
+
+  render() {
+    const filterText = this.props.filterText
+    const rows = []
+    this.state.names.forEach(names => {
+      if (!names.name.toLowerCase().startsWith(filterText.toLowerCase())) {
+        return
+      }
+      else
+      {
+        rows.push(
+          <Names key={names.name} name={names.name} />
+        )
+      }
+    })
+    return (
+      <ul style={filterText ? show : hide}>{rows}</ul>
+    )
+  }
+}
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleFilterTextChange = this.handleFilterTextChange.bind(this)
+  }
+
+  handleFilterTextChange(event) {
+    this.props.onFilterTextChange(event.target.value)
+  }
+
+  render() {
+    const filterText = this.props.filterText
+    return (
+      <form>
+        <input
+          type="text"
+          placeholder="search"
+          value={filterText}
+          onChange={this.handleFilterTextChange}
+        />
+      </form>
+    )
+  }
+}
+
+class FilterableNameTable extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      filterText: ''
+    }
+    this.handleFilterTextChange = this.handleFilterTextChange.bind(this)
+  }
+
+  handleFilterTextChange(filterText) {
+    this.setState({
+      filterText: filterText
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar
+          filterText={this.state.filterText}
+          onFilterTextChange={this.handleFilterTextChange}
+        />
+        <NameTable filterText={this.state.filterText} />
+      </div>
+    )
+  }
+}
+
+render(<FilterableNameTable />, document.querySelector('#app'))
